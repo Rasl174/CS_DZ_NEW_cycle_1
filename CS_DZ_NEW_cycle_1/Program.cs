87cec75f5a1aceab10e0b813c3e9baa14b1c0f8f
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-03-11 13:17:05 +0500

    Изменил

--- CS_DZ_NEW_cycle_1/Program.cs
+++ CS_DZ_NEW_cycle_1/Program.cs
@@ -11,15 +11,21 @@ namespace CS_DZ_NEW_cycle_1
         static void Main(string[] args)
         {
             int countNumbers = 0;
-            int threeDigitNumbers;
+            int N;
 
-            for (int N = 1; N <= 27; N++)
+            for (int i = 1; i < 10; i++)
             {
-                threeDigitNumbers = N * N;
-
-                if (threeDigitNumbers > 99 && threeDigitNumbers <= 999)
+                for (int j = 0; j < 10; j++)
                 {
-                    countNumbers++;
+                    for (int k = 0; k < 10; k++)
+                    {
+                        N = i + j + k;
+
+                        if (N >= 1 && N <= 27)
+                        {
+                            countNumbers++;
+                        }
+                    }
                 }
             }
             Console.WriteLine(countNumbers);
